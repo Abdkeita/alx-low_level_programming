e6342defce55b1bb63df68c7fd84c7df63ff8c65
abdullah keita <keitaabdullai81@gmail.com>
2021-06-18 21:35:49 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,15 +1,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - this is the only function
+ * Description: It will print the last digit of the number stored in n
+ * Return: The function returns 0
+ */
 int main(void)
 {
-int n;
-
+int n, lastDig;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-/* your code goes there */
+lastDig = n % 10;
+if (lastDig > 5)
+{
+printf("Last digit of %d is %d and is greater than 5\n", n, lastDig);
+}
+else if (lastDig == 0)
+{
+printf("Last digit of %d is %d and is 0\n", n, lastDig);
+}
+else if (lastDig < 6 && !lastDig == 0)
+{
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDig);
+}
 return (0);
 }
