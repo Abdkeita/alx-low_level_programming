14b2605444fe42455aaa1ce5d1fb112a4776bb04
abdullah keita <keitaabdullai81@gmail.com>
2021-06-20 23:54:19 +0100

    Create 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - This is the main function
+ * Description: Prints out the alphabet in lowercase followed by a new line
+ * Return: The function returns 0
+ */
+int main() {
+int num = '0';
+char c;
+
+while (num <= '9')
+{
+putchar(num);
+num++;
+if(num > '9')
+{
+for( c = 'a'; c <= 'f'; c++)
+putchar(c);
+putchar('\n');
+return 0;
+}
+}
+}
