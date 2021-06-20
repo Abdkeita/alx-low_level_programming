b0e672c338fe61cfac0b04918a3f8f4615bb8d46
Abdkeita <keitaabdullai81@gmail.com>
2021-06-20 15:27:56 -0700

    print single digit from 0

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - this is the main function
+ *description: prints out single digit number in base 10 from 0
+ *return: the function returns 0
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n <= 9; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
