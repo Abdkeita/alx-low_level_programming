1515297472c360df69c61066e73cd04f6f810697
Abdkeita <keitaabdullai81@gmail.com>
2021-06-29 12:51:43 -0700

    pointers two update

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * swap_int - takes two pointer to a parameter and updates its value.
+ * @a: first input integer.
+ * @b: second input integer.
+ * Return: no return.
+ */
+void swap_int(int *a, int *b)
+{
+*a = 98;
+*b = 42;
+}
